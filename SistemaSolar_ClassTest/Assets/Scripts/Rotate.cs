8c22097ee3f6faf3b9bde2c6cc50cc64e7653f84
David Uribe <daviduribevfx@gmail.com>
2024-03-22 13:02:55 +0100

    New  scripts Scripts/VOID.cs

--- SistemaSolar_ClassTest/Assets/Scripts/Rotate.cs
+++ SistemaSolar_ClassTest/Assets/Scripts/Rotate.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotación : MonoBehaviour
+public class Rotate : MonoBehaviour
 {
     [SerializeField]
     float velRotacion, velOrbitacion;
---
+++ SistemaSolar_ClassTest/Assets/Scripts/VOID.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VOID : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      //note for testing  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
